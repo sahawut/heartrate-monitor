ddb70ee84e0a05a1ca1c4d274770233514baefa6
sahawut <sahawut@gmail.com>
2020-07-25 11:21:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     heartbeat = true
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(inputAnalog)
+    trace2.scrollImage(1, delay)
+})
 input.onGesture(Gesture.Shake, function () {
     heartbeat = true
 })
+let inputAnalog = 0
 let heartbeat = false
+let trace2: Image = null
+let delay = 0
+delay = 100
 let trace1 = images.createImage(`
     # . . . .
     # . . . .
@@ -12,7 +20,7 @@ let trace1 = images.createImage(`
     # # . . .
     # # # # #
     `)
-let trace2 = images.createImage(`
+trace2 = images.createImage(`
     . . . . .
     . . . . .
     . . . . .
@@ -21,13 +29,14 @@ let trace2 = images.createImage(`
     `)
 trace2.showImage(0)
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P1) > 700) {
+    inputAnalog = pins.analogReadPin(AnalogPin.P1)
+    if (inputAnalog > 700) {
         heartbeat = true
     }
     if (heartbeat) {
         heartbeat = false
         music.playTone(262, music.beat(BeatFraction.Half))
-        trace1.scrollImage(1, 200)
-        trace2.scrollImage(1, 200)
+        trace1.scrollImage(1, delay)
+        trace2.scrollImage(1, delay)
     }
 })
